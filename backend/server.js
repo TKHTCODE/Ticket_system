2b2aa785555f023a8585c01666294327fd8dec66
TKHTS <sakuranohanabira1269@gmail.com>
2022-05-19 16:47:52 -0700

    Change setting when production

--- backend/server.js
+++ backend/server.js
@@ -23,16 +23,16 @@ app.use('/api/tickets', require('./routes/ticketRoutes'))
 
 
 //Serve Frontend
-if(process.env.NODE_ENV === 'production'){
-    //Set build folder as static
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+// if(process.env.NODE_ENV === 'production'){
+//     //Set build folder as static
+//     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build','index.html' ))
-}else {
-    app.get('/', (req,res) => {
-        res.status(200).json({message: 'Welcome to the support desk api'})
-    })
-}
+//     app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build','index.html' ))
+// }else {
+//     app.get('/', (req,res) => {
+//         res.status(200).json({message: 'Welcome to the support desk api'})
+//     })
+// }
 
 app.use(errorHandler)
 
