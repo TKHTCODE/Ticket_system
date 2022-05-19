8aca1651ebff6c0863d360fdd718e0b20eb4cf77
TKHTS <sakuranohanabira1269@gmail.com>
2022-05-19 12:49:38 -0700

    Add function to display note

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,64 +1,103 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { toast } from 'react-toastify'
-import { useSelector, useDispatch } from 'react-redux'
-import { getTicket, reset, closeTicket } from '../features/tickets/ticketSlice'
-import { useParams, useNavigate } from 'react-router-dom'
-import BackButton from '../components/BackButton'
-import Spinner from '../components/Spinner'
+import React from "react";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+import Modal from 'react-modal'
+import { useSelector, useDispatch } from "react-redux";
+import { getTicket, closeTicket } from "../features/tickets/ticketSlice";
+import { getNotes, reset as notesReset } from "../features/notes/noteSlice";
+import { useParams, useNavigate } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import NoteItem from "../components/NoteItem";
 
-function Ticket() {
-    const { ticket, isLoading, isSuccess, isError, message } =
-        useSelector((state) => state.tickets)
-
-    const params = useParams()
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const { ticketId } = useParams()
-
-    useEffect(() => {
-        if (isError) {
-            toast.error(message)
-        }
-
-        dispatch(getTicket(ticketId))
-        // eslint-disable-next-line
-    }, [isError, message, ticketId])
-
-    //Close ticket
-    const onTicketClose = () => {
-        dispatch(closeTicket(ticketId))
-        toast.success('Ticket Closed')
-        navigate('/tickets')
-    }
-
-    if (isLoading) {
-        return <Spinner />
-    }
-
-    if (isError) {
-        return <h3>Something went wrong</h3>
-    }
-
-    return <div className='ticket-page'>
-        <header className="ticket-header">
-            <BackButton url='/tickets' />
-            <h2>Ticket ID: {ticket._id}
-                <span className={`status status-${ticket.status}`}>
-                    {ticket.status}
-                </span>
-            </h2>
-            <h3>Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-US')}</h3>
-            <h3>Product: {ticket.product}</h3>
-            <hr />
-            <div className='ticket-desc'>
-                <h3>Description of Issue</h3>
-                <p>{ticket.description}</p>
-            </div>
-        </header>
-
-         {ticket.status !== 'closed' && (<button onClick={onTicketClose} className="btn btn-block btn-danger">Close Ticket</button>)}
-    </div>
+const customStyles = {
+    content:{
+        width: '600px',
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        position: 'relative'
+    },
 }
 
-export default Ticket
+Modal.setAppElement('#root')
+
+function Ticket() {
+  const { ticket, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.tickets
+  );
+
+  const { notes, isLoading: notesIsLoading } = useSelector(
+    (state) => state.notes
+  );
+
+  const params = useParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { ticketId } = useParams();
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    dispatch(getTicket(ticketId));
+    dispatch(getNotes(ticketId));
+    // eslint-disable-next-line
+  }, [isError, message, ticketId]);
+
+  //Close ticket
+  const onTicketClose = () => {
+    dispatch(closeTicket(ticketId));
+    toast.success("Ticket Closed");
+    navigate("/tickets");
+  };
+
+  if (isLoading || notesIsLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h3>Something went wrong</h3>;
+  }
+
+  return (
+    <div className="ticket-page">
+      <header className="ticket-header">
+        <BackButton url="/tickets" />
+        <h2>
+          Ticket ID: {ticket._id}
+          <span className={`status status-${ticket.status}`}>
+            {ticket.status}
+          </span>
+        </h2>
+        <h3>
+          Date Submitted: {new Date(ticket.createdAt).toLocaleString("en-US")}
+        </h3>
+        <h3>Product: {ticket.product}</h3>
+        <hr />
+        <div className="ticket-desc">
+          <h3>Description of Issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+        <h2>Notes</h2>
+      </header>
+
+
+      {notes.map((note) => (
+        <NoteItem key={note._id} note={note} />
+      ))}
+
+      {ticket.status !== "closed" && (
+        <button onClick={onTicketClose} className="btn btn-block btn-danger">
+          Close Ticket
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default Ticket;
