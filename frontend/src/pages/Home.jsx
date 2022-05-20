f01b32d8bd8a1f87c320bccc0e1b46d8c7472135
TKHTS <sakuranohanabira1269@gmail.com>
2022-05-19 19:13:20 -0700

    Fix logout and home layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import {FaQuestionCircle, FaTicketAlt} from 'react-icons/fa'
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
 
 function Home() {
-  return (
-    <>
-    <section className="heading">
-      <h1>What do you need help with?</h1>
-      <p>Please choose from an option below</p>
-    </section>
-    <Link to='/new-ticket' className='btn btn-reverse btn-block'>
-      <FaQuestionCircle />Create New Ticket
-    </Link>
+  const isLoggedIn = localStorage.getItem("user");
+  if (isLoggedIn !== null) {
+    return (
+      <>
+        <section className="heading">
+          <h1>What do you need help with?</h1>
+          <p>Please choose from an option below</p>
+        </section>
+        <Link to="/new-ticket" className="btn btn-reverse btn-block">
+          <FaQuestionCircle />
+          Create New Ticket
+        </Link>
 
-    <Link to='/tickets' className='btn btn-block'>
-      <FaTicketAlt />View My Tickets
-    </Link>
-    </>
-  )
+        <Link to="/tickets" className="btn btn-block">
+          <FaTicketAlt />
+          View My Tickets
+        </Link>
+      </>
+    );
+  } else {
+    return <h1>Welcome to Support ticket sytem</h1>;
+  }
 }
 
-export default Home
+export default Home;
